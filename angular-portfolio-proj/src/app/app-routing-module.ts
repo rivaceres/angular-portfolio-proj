5e16d415c02ffc7f1790e9bebfd663d7960cd98e
Riva Ceres Laoreno <rivacerslao@gmail.com>
2025-09-24 18:05:02 +0800

    Fix header bug. Now transparent and is in correct routing.

--- angular-portfolio-proj/src/app/app-routing-module.ts
+++ angular-portfolio-proj/src/app/app-routing-module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home';
+import { About } from './pages/about';
+import { Projects } from './pages/projects';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+  { path: 'projects', component: Projects },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'ignore' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
